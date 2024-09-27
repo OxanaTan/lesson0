7c827a9b504636ea21e0e259a783cb8d0d0351c7
OxanaTan <oxanatan5@gmail.com>
2024-09-27 21:54:29 +0500

    Add files via upload

--- module_1_3..py
+++ module_1_3..py
@@ -2,8 +2,8 @@ name = "Oxana"
 print(name)
 age = 30
 print(age)
-new_age = "31"
+new_age = (age + 1)
 print(new_age)
-is_stident = (age < int(new_age))
+is_stident = (age < new_age)
 print(is_stident)
 
