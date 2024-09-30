a32d9bf4466647c99670d98d59158610d6467a29
OxanaTan <oxanatan5@gmail.com>
2024-09-30 22:15:43 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, "c", "d", False)
+print(immutable_var)
+
+# immutable_var переменная являетяся кортежем (неизменяемым типом данных)
+
+mutable_list = ["winter", "summer", "spring", "autumn"]
+mutable_list[0] = "summer"
+print(mutable_list)
