c81f8a0f28f1f60a683d7090b80b7e349ce7eaa7
OxanaTan <oxanatan5@gmail.com>
2025-01-15 14:34:19 +0500

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,23 +1,23 @@
-def password(n):
-    password = ""
+def generate_password(n):
+    result = ""
     pairs_used = set()
 
     for i in range(1, 21):
-        for j in range(i, 21):
-            if i + j <= n * 2:
-                pair_sum = i + j
-                if n % pair_sum == 0:
-                    pair = (i, j)
-                    if pair not in pairs_used:
-                        pairs_used.add(pair)
-                        password += f"{i}{j}"
+        for j in range(i + 1, 21):
+            pair_sum = i + j
 
-    return password
+            if n % pair_sum == 0:
+                pair = (i, j)
+                if pair not in pairs_used:
+                    pairs_used.add(pair)
+                    result += f"{i}{j}"
+
+    return result
 
 
 n = int(input("Введите число от 3 до 20: "))
 if 3 <= n <= 20:
-    your_password = password(n)
-    print(f"Пароль для числа {n}: {your_password}")
+    password = generate_password(n)
+    print(f"Пароль для числа {n}: {password}")
 else:
     print("Число должно быть от 3 до 20.")
